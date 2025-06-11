7120060c522568036c367f8eea7025c26f367cd0
Varun <varun.gavoor@7edge.com>
2025-06-11 10:15:10 +0530

    Third commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [ fileChanged, setFileChanged ] = useState(false);
-  const handleFileChange = () => {
-    setFileChanged(true);
-  }
+  const [fileChanged, setFileChanged] = useState(false);
 
+  const handleFileChange = (e) => {
+    setFileChanged(true);
+    console.log('Selected file:', e.target.files[0]);
+  };
 
   return (
-    <div  className='flex justify-center pt-[25%]'>
-    <input type="file" placeholder='Please put your file here' onClick={handleFileChange} className=''/>
+    <div className="flex justify-center pt-[25%]">
+      <input
+        type="file"
+        placeholder="Please put your file here"
+        onChange={handleFileChange}
+        className="border p-2"
+      />
+      {fileChanged && <p className="ml-4 text-green-600">File selected!</p>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
